9487b6779b1831e1582fcad50cbdbd655dc1cf0d
Max Fu <mfu27@ucmerced.edu>
2024-08-17 22:23:27 -0700

    working brute force solution to question 1299, pattern array

--- 01_array_and_hashing/replace_elements.cpp
+++ 01_array_and_hashing/replace_elements.cpp
@@ -1,22 +1,22 @@
+// Working Solution 
+// Question 1299. Replace Elements with Greatest Element on Right Side. 
+
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
 
         int size = arr.size(); 
-        int current_value = arr[0]; // Starting with the value to the right of the first value 
         
         for(int i = 1; i <= size - 1; i++) { 
+            int current_value = arr[i]; // Starting with the value to the right of the first value 
             for(int j = i; j <= size - 1; j++) { 
                 if(current_value < arr[j]) { 
                     current_value = arr[j]; 
                 } 
-                arr[i] = current_value;    
-            }
+            } 
+            arr[i - 1] = current_value;    
         } 
-
         arr[size-1] = -1;  
         return arr;  
     }
-}; 
-
-
+}
