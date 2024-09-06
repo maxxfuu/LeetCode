5eda1744602e1552f2b908cd072d5bc780fb81bb
Max Fu <mfu27@ucmerced.edu>
2024-09-06 00:37:52 -0700

    completed merge two sorted list leetcode

--- 05_linked_list/singly_linked_list.cpp
+++ 05_linked_list/singly_linked_list.cpp
@@ -10,7 +10,46 @@ class ListNode { // Creating a class called ListNode
     }
 
 
-}; 
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0); 
+        ListNode* ptr = &dummy; 
+
+        while(list1 && list2) { 
+            if(list1->val_ < list2->val_) { 
+                ptr->next = list1; 
+                list1 = list1->next; 
+            } else { 
+                ptr->next = list2; 
+                list2 = list2->next; 
+            }
+            ptr = ptr->next; 
+        } 
+
+        if(list1) { 
+            ptr->next = list1; 
+        } else { 
+            ptr->next = list2; 
+        }
+
+        return dummy.next; 
+    }
+};
+
 
 
 int main(int argc, char* argv[]) { 
