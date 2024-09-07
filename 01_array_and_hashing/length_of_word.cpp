40b5cfa752f531531739347906e6a81a81c58ecf
Max Fu <mfu27@ucmerced.edu>
2024-09-06 23:23:52 -0700

    new solution

---
+++ 01_array_and_hashing/length_of_word.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int size = s.size() - 1; 
+        int count = 0; 
+        for(int i = size; i >= 0; i--) { 
+            if(s[i] == ' ') { 
+                size--; 
+            } else {
+                break; 
+            }
+        } 
+
+        for(int j = size; j >= 0; j--) { 
+            if(s[j] != ' ') {
+                count++; 
+            } else { 
+                break; 
+            }
+        } 
+
+        return count; 
+    }
+};
